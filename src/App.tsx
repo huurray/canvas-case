775d00051dd2bdc3225014532c1aad13b5175298
Ray(허준혁) <ray.heo@bunjang.co.kr>
2021-11-30 22:28:56 +0900

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,17 @@ import reset from "styles/reset";
 import theme from "styles/theme";
 
 const App = () => (
-  <div>12312312</div>
-  // <ThemeProvider theme={theme}>
-  //   <Provider store={rootStore}>
-  //     <Global styles={reset} />
-  //     <Router>
-  //       <Routes>
-  //         <Route path="/" element={<Main />} />
-  //         <Route path="/make-circles" element={<MakeCircles />} />
-  //       </Routes>
-  //     </Router>
-  //   </Provider>
-  // </ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <Provider store={rootStore}>
+      <Global styles={reset} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/make-circles" element={<MakeCircles />} />
+        </Routes>
+      </Router>
+    </Provider>
+  </ThemeProvider>
 );
 
 export default App;
